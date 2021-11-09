98b980869d34b21cbd99e1429a64ae514854fc46
JaisonFunderburker <80485419+JaisonFunderburker@users.noreply.github.com>
2021-11-09 11:00:57 +0100

    folder layout created

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="resources/css/style.css">
+    <title></title>
+  </head>
+  <body>
+    <h1>hello world!</h1>
+  </body>
+</html>
