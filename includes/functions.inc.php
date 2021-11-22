a4c45bb06c8dee5b32d6ce0c671729d3138178ec
Boughten52 <jonatanstenlund99@gmail.com>
2021-11-22 16:43:14 +0100

    Added profile

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -53,6 +53,55 @@
     exit();
   }
 
+  function editUser($conn, $newFname, $newLname, $newEmail, $newPwd, $oldPwd, $uid) {
+    $userArr = fetch_user($conn, $uid, null);
+
+    //Make sure the input old password matches the stored password
+    if (!checkPassword($oldPwd, $userArr["pwd_salt"], $userArr["pwd_sha256"])) {
+      header("location: ../pages/profile?error=WRONG_PASSWORD");
+      exit();
+    }
+
+    //Make sure the input new password doesn't match the stored password
+    if ($oldPwd == $newPwd) {
+      header("location: ../pages/profile?error=NEW_PASSWORD_SAME_AS_OLD");
+      exit();
+    }
+
+    $salt = null;
+    if ($newFname == null){
+      $newFname = $userArr["fname"];
+    }
+    if ($newLname == null){
+      $newLname = $userArr["lname"];
+    }
+    if ($newEmail == null){
+      $newEmail = $userArr["email"];
+    }
+    if ($newPwd == null){
+      $newPwd = $oldPwd;
+      $salt = $userArr["pwd_salt"];
+    }
+    else{
+      $salt = random_bytes(32);
+    }
+
+    $sql = "UPDATE user SET fname=?, lname=?, email=?, pwd_sha256=?, pwd_salt=? WHERE id=?";
+    $stmt = mysqli_stmt_init($conn);
+
+    if (!mysqli_stmt_prepare($stmt, $sql)){
+      header("location: ../pages/profile?error=STMT_FAILED");
+      exit();
+    }
+
+    mysqli_stmt_bind_param($stmt, "ssssss", $newFname, $newLname, $newEmail, hash("sha256", $newPwd . $salt), $salt, $userArr["id"]);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+
+    header("location: ../pages/profile?error=none");
+    exit();
+  }
+
   function loginUser($conn, $email, $pwd) {
     $userArr = fetch_user($conn, null ,$email);
 
@@ -74,6 +123,10 @@
       header("location: ../index.php");
       exit();
     }
+  }
+
+  function checkPassword($raw_pwd, $raw_salt, $hashed_pwd) {
+    return 0 == strcmp(hash("sha256", $raw_pwd . $raw_salt), $hashed_pwd);
   }
 
  ?>
