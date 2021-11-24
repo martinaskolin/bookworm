c517305dff777c75e0ceea41cba88337b18701b1
Boughten52 <jonatanstenlund99@gmail.com>
2021-11-24 23:41:31 +0100

    Added stylesheets to login and signup

--- pages/login/index.php
+++ pages/login/index.php
@@ -3,19 +3,25 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/bookworm/pages/login/style.css">
     <title></title>
   </head>
   <body>
 
     <?php include_once '../../includes/header.inc.php'; ?>
 
-    <section class="login-form">
-      <form action="../../includes/login.inc.php" method="post">
-        <input type="text" name="email" placeholder="Email...">
-        <input type="password" name="pwd" placeholder="Password...">
-        <button type="submit" name="submit">Log In</button>
-      </form>
-    </section>
+    <div class="login-div">
+      <div class="login-header">
+        <p>Log In To Account</p>
+      </div>
 
+      <section class="login-form">
+        <form action="../../includes/login.inc.php" method="post">
+          <input type="text" name="email" placeholder="Email...">
+          <input type="password" name="pwd" placeholder="Password...">
+          <button type="submit" name="submit">Log In</button>
+        </form>
+      </section>
+    </div>
   </body>
 </html>
