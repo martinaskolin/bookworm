b7139235136651d8e517113bdce6494f10a46106
sojon03 <78483531+sojon03@users.noreply.github.com>
2021-12-07 10:51:54 +0100

    Något fungerande cart

--- pages/cart/index.php
+++ pages/cart/index.php
@@ -14,20 +14,22 @@
           require_once '../../includes/functions.inc.php'; ?>
 
     <?php
-
     if (isset($_SESSION["uid"])){
-      //echo "<h1> Came to false statement </h1>";
       echo "<h1> Shopping Cart </h1>";
+
       $sql = "SELECT p.* FROM cart_item ci, product p WHERE p.id = ci.pid;";
       $result = mysqli_query($conn, $sql);
 
       if (mysqli_num_rows($result) > 0) {
-        while($row = mysqli_fetch_assoc($result)){
-        echo "<p> Product id is: " . $row['id'] . " product name is " . $row['name'] . " </p>";
-        //echo "<a href='/bookworm/includes/addtocart.inc.php?id=" . $row['price'] . " <i class='bi-bag-fill'></i>";
+        $sum = 0;
+        while($item = mysqli_fetch_assoc($result)){
+        echo "<p> Product id is: " . $item['id'] . " product name is " . $item['name'] . " " . $item['price'] . " kr </p>";
+        $sum = $sum + $item['price'];
       }
+      echo "<p> Total cost is: "  . $sum . " kr </p>";
 
       }
+
       else {
         echo "<p> No items in your cart yet </p>";
       }
